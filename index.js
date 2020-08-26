aa6fa1afc2de036768730a0fb3fe7ffa249849e6
vincentyang01 <vincentyangcsi@gmail.com>
2020-08-26 16:23:04 -0400

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const moveContainer = document.getElementById("move-containier")
+  
+  var moveList = []
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key == "ArrowUp"){
+      moveList.push(move("up"))
+      moveContainer.push("<li>Up</li>")
+    }
+    else if (e.key == "ArrowDown"){
+      moveList.push(move("down"))
+      moveContainer.push("<liDown</li>")
+    }
+    else if (e.key == "ArrowLeft"){
+      moveList.push(move("left"))
+      moveContainer.push("<li>Left</li>")
+    }
+    else (e.key == "ArrowRight")
+    moveList.push(move("right"))
+    moveContainer.push("<li>Right</li>")
+  })
 })
